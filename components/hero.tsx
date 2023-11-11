bc4613abe4185b5f2a7ad454ff2b23a6ec028b62
Marc Apesos <apesos2013@gmail.com>
2023-11-11 10:43:21 -0500

    hero video overlay

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,25 +1,12 @@
-import VideoThumb from '@/public/images/hero-image.png'
-import ModalVideo from '@/components/modal-video'
-
 export default function Hero() {
   return (
-    <section className="relative h-screen">
-
-      {/* Illustration behind hero content */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
-        <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
-              <stop stopColor="#FFF" offset="0%" />
-              <stop stopColor="#EAEAEA" offset="77.402%" />
-              <stop stopColor="#DFDFDF" offset="100%" />
-            </linearGradient>
-          </defs>
-
-        </svg>
-      </div>
-
-      <video
+    <section className="relative h-screen flex justify-center ">
+      <div className="video-container min-w-full relative h-full object-cover">
+        <div
+          style={{ backgroundColor: "#a13073" }}
+          className="overlay h-full w-full absolute top-0 left-0 opacity-30"
+        ></div>
+        <video
           autoPlay
           playsInline
           muted
@@ -27,27 +14,56 @@ export default function Hero() {
           controls={false}
           id="backgroundVideo"
           poster="mj4d-background-thumb.png"
-          className="min-w-full absolute h-full object-cover"
+          className="min-w-full absolute h-full object-cover -z-1"
         >
           <source src="/videos/hero_vid.mp4" type="video/webm"></source>
           <source src="/videos/hero_vid.mp4" type="video/mp4"></source>
-          {/* MP4 needs to be H264 encoded for Safari compat; see https://developer.apple.com/documentation/webkit/delivering_video_content_for_safari */}
+          {/* /* MP4 needs to be H264 encoded for Safari compat; see https://developer.apple.com/documentation/webkit/delivering_video_content_for_safari */}
         </video>
+      </div>
 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        {/* Hero content */}
-        <div className="pt-40 pb-12 md:pt-90 md:pb-20">
-          {/* Section header */}
-          <div className="text-center pb-4 md:pb-4">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-yellow-400">idk lol</span>
-            <br></br>
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">lmao</span>
-            </h1>
-          </div>
-
-        </div>
+      {/* Socials*/}
+      <div className="absolute top-1/4 w-full flex flex-col justify-center items-center">
+        <img
+          src="/images/GM-Logo_multicolor.png"
+          alt="Guess Method Main Logo"
+          className="hero-logo mb-12 lg:w-1/4 w-3/4"
+        />
+        <nav
+          className="
+            links      
+            w-full
+            flex
+            justify-center
+            gap-10
+          "
+        >
+          <a
+            href="https://open.spotify.com/artist/1wJqYDH9LsyTL8HQstd6CD?si=c4s19_FJSW-rKf9g3l2NDQ&fbclid=IwAR3ESsAfRbepxBAKqhO2P8Fzbxf8vzU1GGHBX194-I2RCtVHl10mHHM09uM"
+            target="_blank"
+          >
+            <img
+              className="lg:w-14 lg:h-14 w-12 h-12"
+              src="images/socials/spotify.png"
+              alt="link to Guess Method spotify profile"
+            />
+          </a>
+          <a href="https://www.facebook.com/search/top?q=pipin" target="_blank">
+            <img
+              className="lg:w-14 lg:h-14 w-12 h-12"
+              src="images/socials/facebook.png"
+              alt="link to Guess Method Facebook profile"
+            />
+          </a>
+          <a href="https://www.instagram.com/pipin.hot.music/" target="_blank">
+            <img
+              className="lg:w-14 lg:h-14 w-12 h-12"
+              src="images/socials/instagram.png"
+              alt="link to Guess Method instagram profile"
+            />
+          </a>
+        </nav>
       </div>
     </section>
-  )
+  );
 }
